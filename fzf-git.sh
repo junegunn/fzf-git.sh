6c6fc65c54bc4ea1a4dd7be0479da6fae3ebfe83
Junegunn Choi <junegunn.c@gmail.com>
2022-08-31 09:06:12 +0900

    Update fzf-git.sh

--- fzf-git.sh
+++ fzf-git.sh
@@ -132,7 +132,7 @@ fi
 _fzf_git_files() {
   _fzf_git_check || return
   (git -c color.status=always status --short
-   git ls-files | (excludes="$(git status -s | grep '^[^?]' | cut -c4-)"; [ -n "${excludes}" ] && grep -vf <(echo "${excludes}") || cat) | sed 's/^/   /') |
+   git ls-files | grep -vxFf <(git status -s | grep '^[^?]' | cut -c4-; echo :) | sed 's/^/   /') |
   _fzf_git_fzf -m --ansi --nth 2..,.. \
     --prompt '📁 Files> ' \
     --header $'CTRL-O (open in browser) ╱ ALT-E (open in editor)\n\n' \

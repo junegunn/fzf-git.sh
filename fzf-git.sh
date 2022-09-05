20374e703082faeb15b006192f524b4c1c6b90e1
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-09-05 23:35:25 +0800

    fix: select the hash only and not those look alike in commit title

--- fzf-git.sh
+++ fzf-git.sh
@@ -180,7 +180,7 @@ _fzf_git_hashes() {
     --bind 'ctrl-d:execute:grep -o "[a-f0-9]\{7,\}" <<< {} | head -n 1 | xargs git diff > /dev/tty' \
     --color hl:underline,hl+:underline \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | head -n 1 | xargs git show --color=always' "$@" |
-  grep -o "[a-f0-9]\{7,\}"
+  grep -o "[a-f0-9]\{7,\}" | head -n 1
 }
 
 _fzf_git_remotes() {

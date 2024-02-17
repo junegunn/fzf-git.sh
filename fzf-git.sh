ee1c9d42f0b7dd3c24f638356570eff53ee2ee5c
Junegunn Choi <junegunn.c@gmail.com>
2024-02-17 09:02:21 +0900

    Fix preview error for ambiguous branch names
    ambiguous argument: both revision and filename

--- fzf-git.sh
+++ fzf-git.sh
@@ -159,7 +159,7 @@ _fzf_git_branches() {
     --bind 'ctrl-/:change-preview-window(down,70%|hidden|)' \
     --bind "ctrl-o:execute-silent:bash $__fzf_git branch {}" \
     --bind "alt-a:change-prompt(🌳 All branches> )+reload:bash \"$__fzf_git\" all-branches" \
-    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' "$@" |
+    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) --' "$@" |
   sed 's/^..//' | cut -d' ' -f1
 }
 

cd8cf67307eb72573eb34c2a9e90694d0946a1b7
sakamossan <sakamossan@users.noreply.github.com>
2024-03-22 16:47:39 +0900

    Make _fzf_git_files list all files in the repository (#32)
    Co-authored-by: Junegunn Choi <junegunn.c@gmail.com>

--- fzf-git.sh
+++ fzf-git.sh
@@ -136,13 +136,18 @@ fi
 
 _fzf_git_files() {
   _fzf_git_check || return
+  local root query
+  root=$(git rev-parse --show-toplevel)
+  [[ $root != "$PWD" ]] && query='!../ '
+
   (git -c color.status=always status --short --no-branch
-   git ls-files | grep -vxFf <(git status -s | grep '^[^?]' | cut -c4-; echo :) | sed 's/^/   /') |
+   git ls-files "$root" | grep -vxFf <(git status -s | grep '^[^?]' | cut -c4-; echo :) | sed 's/^/   /') |
   _fzf_git_fzf -m --ansi --nth 2..,.. \
     --border-label '📁 Files' \
     --header $'CTRL-O (open in browser) ╱ ALT-E (open in editor)\n\n' \
     --bind "ctrl-o:execute-silent:bash $__fzf_git file {-1}" \
     --bind "alt-e:execute:${EDITOR:-vim} {-1} > /dev/tty" \
+    --query "$query" \
     --preview "git diff --no-ext-diff --color=always -- {-1} | sed 1,4d; $_fzf_git_cat {-1}" "$@" |
   cut -c4- | sed 's/.* -> //'
 }

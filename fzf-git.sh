0a4c0e33b520a68755bbcb283e07bbd03caca964
LangLangbart <92653266+LangLangBart@users.noreply.github.com>
2024-07-02 19:22:16 +0200

    refactor: rename FZF_GIT_PAGER && __fzf_git_pager and simplify function

--- fzf-git.sh
+++ fzf-git.sh
@@ -50,18 +50,10 @@ __fzf_git_cat() {
   fi
 }
 
-__fzf_pager() {
-  local core_pager
-  core_pager=$(git config --get core.pager)
-  if [[ -n $FZF_PAGER ]]; then
-    echo "$FZF_PAGER"
-  elif [[ -n $GIT_PAGER ]]; then
-    echo "$GIT_PAGER"
-  elif [[ -n $core_pager ]]; then
-    echo "$core_pager"
-  else
-    echo cat
-  fi
+__fzf_git_pager() {
+  local pager
+  pager="${FZF_GIT_PAGER:-${GIT_PAGER:-$(git config --get core.pager 2>/dev/null)}}"
+  echo "${pager:-cat}"
 }
 
 if [[ $# -eq 1 ]]; then
@@ -189,7 +181,7 @@ _fzf_git_files() {
     --bind "ctrl-o:execute-silent:bash $__fzf_git file {-1}" \
     --bind "alt-e:execute:${EDITOR:-vim} {-1} > /dev/tty" \
     --query "$query" \
-    --preview "git diff --no-ext-diff --color=$(__fzf_git_color .) -- {-1} | $(__fzf_pager); $(__fzf_git_cat) {-1}" "$@" |
+    --preview "git diff --no-ext-diff --color=$(__fzf_git_color .) -- {-1} | $(__fzf_git_pager); $(__fzf_git_cat) {-1}" "$@" |
   cut -c4- | sed 's/.* -> //'
 }
 
@@ -217,7 +209,7 @@ _fzf_git_tags() {
     --border-label '📛 Tags' \
     --header $'CTRL-O (open in browser)\n\n' \
     --bind "ctrl-o:execute-silent:bash $__fzf_git tag {}" \
-    --preview "git show --color=$(__fzf_git_color .) {} | $(__fzf_pager)" "$@"
+    --preview "git show --color=$(__fzf_git_color .) {} | $(__fzf_git_pager)" "$@"
 }
 
 _fzf_git_hashes() {
@@ -230,7 +222,7 @@ _fzf_git_hashes() {
     --bind "ctrl-d:execute:grep -o '[a-f0-9]\{7,\}' <<< {} | head -n 1 | xargs git diff --color=$(__fzf_git_color) > /dev/tty" \
     --bind "alt-a:change-border-label(🍇 All hashes)+reload:bash \"$__fzf_git\" all-hashes" \
     --color hl:underline,hl+:underline \
-    --preview "grep -o '[a-f0-9]\{7,\}' <<< {} | head -n 1 | xargs git show --color=$(__fzf_git_color .) | $(__fzf_pager)" "$@" |
+    --preview "grep -o '[a-f0-9]\{7,\}' <<< {} | head -n 1 | xargs git show --color=$(__fzf_git_color .) | $(__fzf_git_pager)" "$@" |
   awk 'match($0, /[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9]*/) { print substr($0, RSTART, RLENGTH) }'
 }
 
@@ -252,7 +244,7 @@ _fzf_git_stashes() {
     --border-label '🥡 Stashes' \
     --header $'CTRL-X (drop stash)\n\n' \
     --bind 'ctrl-x:reload(git stash drop -q {1}; git stash list)' \
-    -d: --preview "git show --color=$(__fzf_git_color .) {1} | $(__fzf_pager)" "$@" |
+    -d: --preview "git show --color=$(__fzf_git_color .) {1} | $(__fzf_git_pager)" "$@" |
   cut -d: -f1
 }
 
@@ -260,7 +252,7 @@ _fzf_git_lreflogs() {
   _fzf_git_check || return
   git reflog --color=$(__fzf_git_color) --format="%C(blue)%gD %C(yellow)%h%C(auto)%d %gs" | _fzf_git_fzf --ansi \
     --border-label '📒 Reflogs' \
-    --preview "git show --color=$(__fzf_git_color .) {1} | $(__fzf_pager)" "$@" |
+    --preview "git show --color=$(__fzf_git_color .) {1} | $(__fzf_git_pager)" "$@" |
   awk '{print $1}'
 }
 

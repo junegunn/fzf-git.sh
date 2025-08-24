5fdc99cd3ab127b19253c4a6d7af67fa9dcba6b5
Chad Paradis <cparadis6191@gmail.com>
2025-08-23 20:11:09 -0400

    Restrict scope of variables in fzf-git.fish

--- fzf-git.fish
+++ fzf-git.fish
@@ -2,10 +2,10 @@ function __fzf_git_fish
     commandline --insert (eval $argv)
 end
 
-set commands branches each_ref files hashes lreflogs remotes stashes tags worktrees
+set --local commands branches each_ref files hashes lreflogs remotes stashes tags worktrees
 
 for command in $commands
-    set key (string sub --end=1 $command)
+    set --function key (string sub --end=1 $command)
 
     eval "bind \cg$key \"__fzf_git_fish 'bash fzf-git.sh --run $command'\""
     eval "bind \cg\c$key \"__fzf_git_fish 'bash fzf-git.sh --run $command'\""

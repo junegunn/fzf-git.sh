bd47de769fa1fd8414e3d77937a3da885f3d8807
Chad Paradis <cparadis6191@gmail.com>
2025-08-23 20:20:55 -0400

    Use absolute path of fzf-git.sh in fzf-git.fish

--- fzf-git.fish
+++ fzf-git.fish
@@ -4,9 +4,12 @@ end
 
 set --local commands branches each_ref files hashes lreflogs remotes stashes tags worktrees
 
+# Get the absolute path to fzf-git.sh to use in the key bindings to avoid
+# having to modify `$PATH`.
+set --local fzf_git_sh_path (realpath (dirname fzf-git.sh))
 for command in $commands
     set --function key (string sub --end=1 $command)
 
-    eval "bind \cg$key \"__fzf_git_fish 'bash fzf-git.sh --run $command'\""
-    eval "bind \cg\c$key \"__fzf_git_fish 'bash fzf-git.sh --run $command'\""
+    eval "bind \cg$key \"__fzf_git_fish 'bash $fzf_git_sh_path/fzf-git.sh --run $command'\""
+    eval "bind \cg\c$key \"__fzf_git_fish 'bash $fzf_git_sh_path/fzf-git.sh --run $command'\""
 end

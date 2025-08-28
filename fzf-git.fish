b0f8185372fb7281f548465001f04f09ce5664c6
Chad Paradis <cparadis6191@gmail.com>
2025-08-27 22:53:12 -0400

    Improve fzf-git.fish quoting to handle spaces in paths

--- fzf-git.fish
+++ fzf-git.fish
@@ -8,9 +8,10 @@ set --local commands branches each_ref files hashes lreflogs remotes stashes tag
 # directory of fzf-git.sh) to use in the key bindings to avoid having to modify
 # `$PATH`.
 set --local fzf_git_sh_path (realpath (status dirname))
+echo $fzf_git_sh_path
 for command in $commands
     set --function key (string sub --end=1 $command)
 
-    eval "bind \cg$key   \"__fzf_git_fish 'bash $fzf_git_sh_path/fzf-git.sh --run $command'\""
-    eval "bind \cg\c$key \"__fzf_git_fish 'bash $fzf_git_sh_path/fzf-git.sh --run $command'\""
+    eval "bind \cg$key   '__fzf_git_fish \"bash \'$fzf_git_sh_path/fzf-git.sh\' --run $command\"'"
+    eval "bind \cg\c$key '__fzf_git_fish \"bash \'$fzf_git_sh_path/fzf-git.sh\' --run $command\"'"
 end

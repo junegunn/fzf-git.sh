ff1369e015a8570f187b8b9f23f0e3f63fb44aa1
Christoph Martin <foss@c14n.ch>
2026-08-06 08:47:20 +0200

    Hashes: Drop pointless ../ prefixes in ALT-F
    By unconditionally prefixing all entries with the relative path to the
    top-level directory in `_fzf_git_tree_files()`, every entry in the
    current working directory gets a pointless prefix, e.g. if the current
    working directory is `foo`, the file `./bar` gets shown as `../foo/bar`
    instead of simply `bar`.
    
    Use `sed` to strip the prefix.

--- fzf-git.sh
+++ fzf-git.sh
@@ -222,11 +222,12 @@ EOF
 _fzf_git_tree_files() {
   _fzf_git_check || return
 
-  local treeish cdup
+  local treeish cdup prefix
   cdup="$(git rev-parse --show-cdup)"
+  prefix="$(git rev-parse --show-prefix)"
   for treeish in "$@"; do
     git diff-tree --root --no-commit-id --name-only --line-prefix="$cdup" "$treeish" -r
-  done | sort -u |
+  done | sort -u | sed "s|^$cdup$prefix||" |
     _fzf_git_fzf -m \
       --border-label "📂 Files in $* " \
       --header 'CTRL-O (open in browser) ╱ ALT-E (open in editor)' \
